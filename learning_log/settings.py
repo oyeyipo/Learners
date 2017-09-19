91c6af7d5872c48e0c588570fd2f1448d0383c16
ehmatthes <olaclassik@gmail.com>
2017-09-19 13:13:48 +0100

    template issue fix 1

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -136,6 +136,16 @@ BOOTSTRAP3 = {
     'include_jquery': True,
 }
 
+
+
+PROJECT_DIR = os.path.dirname(__file__) # this is not Django setting.
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_DIR, "templates"),
+    # here you can add another templates directory if you wish.
+)
+
+
+
 # Heroku settings
 cwd = os.getcwd()
 print("--- CWD ---\n", cwd, "\n---\n")
