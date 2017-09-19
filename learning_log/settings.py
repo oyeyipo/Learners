e79dd2e276d1bc141a9fbd87f889d5fd279ca5b5
ehmatthes <olaclassik@gmail.com>
2017-09-19 10:25:12 +0100

    set static url

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -149,8 +149,12 @@ if os.getcwd() == '/app':
     ALLOWED_HOSTS = ['*']
 
     # Static asset configuration
+
+    STATIC_URL = '/static/'
+    
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
     STATICFILES_DIRS = (
             os.path.join(BASE_DIR, 'static'),
-        )
+    )
